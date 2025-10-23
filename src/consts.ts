2b88bdf8b8dd49fe0ff497c326755f97173229ea
mbabwa <mbabwahsingh@gmail.com>
2025-10-23 19:11:08 -0400

    blog setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,38 +1,38 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
+export const SITE_TITLE = 'mbabwa'
 export const SITE_DESCRIPTION =
 	'A simple theme for personal blog sites, created for Astro framework'
 
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = ''
 
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: 'https://github.com/yashjawale',
+		url: '',
 	},
 	{
 		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/yashjawale/',
+		url: '',
 	},
 	{
 		name: 'X / Twitter',
-		url: 'https://twitter.com/imyashjawale',
+		url: '',
 	},
 	{
 		name: 'Instagram',
-		url: 'https://instagram.com/imyashjawale',
+		url: '',
 	},
 ]
 
 export const WebsiteLinks = [
 	{
 		name: 'Home',
-		url: '/',
+		url: '',
 	},
 	{
 		name: 'Blog',
